d7eb57a3ede6be1a885f4f98cd6519367071becf
Kate Mwende <Kmwende419@gmail.com>
2023-09-05 20:35:43 +0300

    Error handling for 403 endpoint

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -30,3 +30,9 @@ def stats() -> str:
 def unauthorized():
     """Get /api/v1/unauthorized raise a 401 error"""
     abort(401, description="Unauthorized")
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden():
+    """GET /api/v1/forbidden raise a 403 error"""
+    abort(403, description="Forbidden")
