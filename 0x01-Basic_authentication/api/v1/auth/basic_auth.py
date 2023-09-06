00cc0aa22a9afaf321bf30d386e488ac13c4ef12
Kate Mwende <Kmwende419@gmail.com>
2023-09-06 22:32:10 +0300

    Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -35,3 +35,16 @@ class BasicAuth(Auth):
             return decoded_credentials
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """show the user email and password"""
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+        if ':' not in decoded_base64_authorization_header:
+            return (None, None)
+        email, password = decoded_base64_authorization_header.\
+            split(':', 1)
+        return (email, password)
