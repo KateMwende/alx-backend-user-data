2311977c19ea8bf5686987fd38ffb4724e3168f5
Kate Mwende <Kmwende419@gmail.com>
2023-09-06 11:20:18 +0300

    changed AUTH_TYPE to string

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -4,6 +4,7 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
+from api.v1.auth.auth import Auth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -14,7 +15,7 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
-AUTH_TYPE = os.environ.get(AUTH_TYPE)
+AUTH_TYPE = os.environ.get('AUTH_TYPE')
 
 if auth:
     from api.v1.auth.auth import Auth
