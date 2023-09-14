aa5ead7c22e7b7b27e8f8f7472db8eef1846f0b5
Kate Mwende <Kmwende419@gmail.com>
2023-09-14 21:10:58 +0300

    Changed method to post

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -26,7 +26,7 @@ def users() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
-@app.route('/sessions', methods=["GET"], stict_slashes=False)
+@app.route('/sessions', methods=["POST"], stict_slashes=False)
 def login() -> str:
     """implement a login function"""
     email = request.form.get("email")
