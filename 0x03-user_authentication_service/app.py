87c9c282f3458b1ac87619e08d7ea8bfb3b39223
Kate Mwende <Kmwende419@gmail.com>
2023-09-14 21:03:19 +0300

    Log in function

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Flask app"""
 from auth import Auth
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 
 app = Flask(__name__)
 AUTH = Auth()
@@ -15,7 +15,7 @@ def index() -> str:
 
 @app.route("/users", methods=["POST"], strict_slashes=False)
 def users() -> str:
-    """mplement the end-point to register a user"""
+    """implement the end-point to register a user"""
     email = request.form.get('email')
     password = request.form.get('password')
 
@@ -24,6 +24,20 @@ def users() -> str:
         return jsonify({"email": user.email, "message": "user created"})
     except Exception:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=["GET"], stict_slashes=False)
+def login():
+    """implement a login function"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    else:
+        session_id = AUTH.create_session(email)
+        res = jsonify({"email": email, "message": "logged in"})
+        res.set_cookie(session_id)
 
 
 if __name__ == "__main__":
