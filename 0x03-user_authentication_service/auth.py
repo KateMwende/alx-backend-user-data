ae22281f0942e1306ac42703769ab5abb23cf0b0
Kate Mwende <Kmwende419@gmail.com>
2023-09-14 20:38:27 +0300

    added user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -49,7 +49,7 @@ class Auth:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
             return None
-        session_id = _generate_uuid()
+        user.session_id = _generate_uuid()
         return user.session_id
 
     def get_user_from_session_id(self, session_id: str) -> User or None:
