2108791af3dc5e8119be37d5b2359f0e0f5751bf
Kate Mwende <Kmwende419@gmail.com>
2023-09-14 20:18:50 +0300

    Find user by session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -42,3 +42,13 @@ class Auth:
         except NoResultFound:
             return False
         return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+
+    def get_user_from_session_id(self, session_id: str):
+        """find user with session id"""
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        return user
