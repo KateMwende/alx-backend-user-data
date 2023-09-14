3cef5b7e29f51434edbbfcf664d47e066429ff73
Kate Mwende <Kmwende419@gmail.com>
2023-09-14 05:10:35 +0300

    Register users if not existing

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import bcrypt
+from db import DB, User
+from sqlalchemy.orm.exc import NoResultFound
 """Making hash passwords with bcrypt"""
 
 
@@ -9,3 +11,20 @@ def _hash_password(password: str) -> bytes:
     hashed_pwd = bcrypt.hashpw(password.encode('utf-8'),
                                salt)
     return hashed_pwd
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """registering users, return a User object"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+        else:
+            raise ValueError('User {} already exists'.format(email))
